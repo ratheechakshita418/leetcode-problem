82f3fd2f3585a218cf91869cfafe263231c41e77
ratheechakshita418 <ratheechakshita418@gmail.com>
2026-09-07 16:12:31 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (25.38%) - LeetSync

---
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+            int maxAbsoluteSum(vector<int>& A) {
+        int s = 0, mi = 0, ma = 0;
+        for (int& a: A) {
+            s += a;
+            mi = min(mi, s);
+            ma = max(ma, s);
+        }
+        return ma - mi;
+    }
+    
+};
